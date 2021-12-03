5585adad74c7857d4be1aec923e7a515d83780f1
Clement-INS <cvigand31@gmail.com>
2021-12-03 18:09:57 +0100

    v1

--- POO/Chat_App/src/main/java/fr/insa/chat/app/Chat_App/App.java
+++ POO/Chat_App/src/main/java/fr/insa/chat/app/Chat_App/App.java
@@ -15,21 +15,30 @@ import java.io.IOException;
 /**
  * JavaFX App
  */
-public class App extends Application {
 
+
+public class App extends Application {
+	
+	private static Stage stage;
     private static Scene scene;
 
     @Override
-    public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 600, 480);
-        stage.setTitle("ChatApp");
-        stage.setScene(scene);
-      
-        stage.show();
+    public void start(Stage stageIni) throws IOException {
+        scene = new Scene(loadFXML("AccueilLogin"), 600, 360);
+        stage = stageIni;
+        stageIni.setTitle("ChatApp");
+        stageIni.setScene(scene);
+        stageIni.show();
     }
 
     static void setRoot(String fxml) throws IOException {
         scene.setRoot(loadFXML(fxml));
+    }
+    
+    public static void reSize(double largeur, double hauteur) {
+    	stage.setWidth(largeur);
+    	stage.setHeight(hauteur);
+    	stage.centerOnScreen();
     }
 
     private static Parent loadFXML(String fxml) throws IOException {
