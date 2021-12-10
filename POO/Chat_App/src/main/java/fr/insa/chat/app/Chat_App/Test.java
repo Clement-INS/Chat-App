758c7956e28a50ed1ed9f375648386837c101efb
vigand <vigand@localhost>
2021-12-10 12:14:33 +0100

    debut database

--- POO/Chat_App/src/main/java/fr/insa/chat/app/Chat_App/Test.java
+++ POO/Chat_App/src/main/java/fr/insa/chat/app/Chat_App/Test.java
@@ -1,9 +1,12 @@
 package fr.insa.chat.app.Chat_App;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.concurrent.TimeUnit;
 
 public class Test{
 	public static void main(String[] args) {
-		UserModel user = new UserModel("Salut");
+		UserModel u1 = new UserModel("Jack");
+		DTBController d = new DTBController(u1);
 	}
 }
