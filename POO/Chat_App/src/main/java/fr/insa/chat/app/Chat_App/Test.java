16e73671ead39c201f385be2f147d0f32a43840d
clement <clement@pc-66.home>
2022-01-04 18:24:43 +0100

    j'espere que c bien

--- POO/Chat_App/src/main/java/fr/insa/chat/app/Chat_App/Test.java
+++
@@ -1,9 +0,0 @@
-package fr.insa.chat.app.Chat_App;
-
-import java.net.UnknownHostException;
-
-public class Test{
-	public static void main(String[] args) throws UnknownHostException {
-		DTBController.getInstance().remove_DB();
-	}
-}
